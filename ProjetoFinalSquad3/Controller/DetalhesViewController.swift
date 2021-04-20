a34ad59936928b4cbd4ce614b5e99be3af167bbf
Cristiane Goncalves Uliana <cristianeguliana@gmail.com>
2021-04-19 21:26:42 -0300

    tirando seleção da célula na primeira tela

--- ProjetoFinalSquad3/Controller/DetalhesViewController.swift
+++ ProjetoFinalSquad3/Controller/DetalhesViewController.swift
@@ -123,4 +123,5 @@ class DetalhesViewController: UIViewController, DetalhesMoedaDelegate, NSFetched
             }
         }
     }
+ 
 }
--- ProjetoFinalSquad3/Controller/MoedasViewController.swift
+++ ProjetoFinalSquad3/Controller/MoedasViewController.swift
@@ -61,7 +61,6 @@ class MoedasViewController: UIViewController, UITableViewDelegate, UITableViewDa
     }
     
     
-    
     // MARK: - TableView
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -89,14 +88,14 @@ class MoedasViewController: UIViewController, UITableViewDelegate, UITableViewDa
         let controller = storyboard.instantiateViewController(withIdentifier: "detalhesMoedaSelecionada") as! DetalhesViewController
         controller.moedaSelecionada = moedaSelecionada
         show(controller, sender: self)
+        listaMoedas.deselectRow(at: indexPath, animated: true)
     }
+    
 
     
     // MARK: - Request
     
     func makeRequest(completion:@escaping([Criptomoeda]) -> Void) {
-
-
 //        let url = URL(string: ApiRest.TodasAsMoedas)!
         let url = URL(string: "https://rest.coinapi.io/v1/assets?apikey=1F8A5E86-F1C9-41C7-B8BB-9DB1B81FDE7C")!
         //let url = URL(string: "https://6076e5cf1ed0ae0017d6a02f.mockapi.io/api/v1/users")!
